ed2f1a692fc5cd62f1275f0eeefed997cd1af72a
darius vaitkevicius <darius.vaitkevicius@gmail.com>
2025-04-03 12:23:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+shopping_list = ["pienas", "kiaušiniai", "duona", "sviestas"]
+print(shopping_list)
+print(shopping_list[0])
+
+shopping_list[shopping_list.index("duona")] = "bananas"
+print(shopping_list)
+
+shopping_list.insert(0, "obuolys")
+print(shopping_list)
+
+shopping_list.extend(["miltai", "cukrus"])
+print(shopping_list)
+
+print(shopping_list[2:4])
