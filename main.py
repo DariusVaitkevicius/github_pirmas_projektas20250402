5bfa6eea4fd797cea197101138e5dd6a41dc1e06
darius vaitkevicius <darius.vaitkevicius@gmail.com>
2025-04-03 12:45:06 +0300

    pirmas commitas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-shopping_list = ["pienas", "kiaušiniai", "duona", "sviestas"]
+shopping_list = ["pienas", "kiaušiniai", "duona", "sviestas", "majonezas"]
 print(shopping_list)
 print(shopping_list[0])
 
